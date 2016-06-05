666641a6eb73a63f8f89adaef920f1f29a4afde4
DesaiV <vishal.n.desai@gmail.com>
2016-06-04 22:32:09 -0700

    Task 1 update
    added HttpHeaderMethod class

---
+++ src/test/java/com/http/test/test_engg_assignment/HttpHeaderMethod.java
@@ -0,0 +1,48 @@
+package com.http.test.test_engg_assignment;
+
+import static com.jayway.restassured.RestAssured.*;
+import static com.jayway.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import org.testng.annotations.Test;
+
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.response.Header;
+import com.jayway.restassured.response.Headers;
+
+public class HttpHeaderMethod {
+
+	private String baseurl = "http://httpbin.org";
+	private String serviceEndpoint = "/headers";
+
+	@Test
+	public void testRepsponseHeaders() {
+
+		given().
+			head(baseurl + serviceEndpoint).
+		then().log().all().
+			statusCode(200).
+			and().
+			header("Content-Type", "application/json").
+			and().header("Server", "nginx");
+	}
+
+	@Test
+	public void testRequesteHeaders() {
+		Header first = new Header("Headers1", "headerValue1");
+		Header second = new Header("Headers2", "headerValue2");
+		Headers headers = new Headers(first, second);
+
+		given().
+			headers(headers).
+		when().
+			get(baseurl + serviceEndpoint).
+		then().log().all().
+			statusCode(200).
+			and().
+			contentType(ContentType.JSON).body("headers.Headers1", equalTo("headerValue1")).
+			and().
+			contentType(ContentType.JSON).body("headers.Headers2", equalTo("headerValue2"));
+
+	}
+}
